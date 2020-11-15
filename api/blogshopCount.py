3e489e671526152000319496e4d9a94617035df1
gss1919 <gss1919@naver.com>
2020-11-15 20:52:06 +0900

    네이버 api

--- api/blogshopCount.py
+++ api/blogshopCount.py
@@ -16,7 +16,7 @@ def getSearchCount(keyword, URL):
     request.add_header("X-Naver-Client-Id",client_id)
     request.add_header("X-Naver-Client-Secret",client_secret)
     response = urllib.request.urlopen(request)
-    rescode = response.getcode()
+    rescod = response.getcode()
     response_body = response.read()
     jsonString = response_body.decode('utf-8')
     jsonDict = json.loads(jsonString)
